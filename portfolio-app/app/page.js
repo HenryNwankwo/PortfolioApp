52a2aab12ca7943a6699594c59f1baa7922bf5de
Nwankwo Henry <nameisifunanya@gmail.com>
2023-06-30 18:10:31 +0100

    Created a theme provider, extended tailwind to darkmode

--- portfolio-app/app/page.js
+++ portfolio-app/app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <p>Welcome to my portfolio</p>
+      <p>Welcome to my portfolio App</p>
     </main>
   );
 }
---
+++ portfolio-app/app/provider.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+import { ThemeProvider } from 'next-themes';
+
+function Providers({ children }) {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted((prev) => !prev);
+  }, []);
+
+  if (!mounted) {
+    return <>{children}</>;
+  }
+
+  return <ThemeProvider attribute='class'>{children}</ThemeProvider>;
+}
+
+export default Providers;
