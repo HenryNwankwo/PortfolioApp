28230b6d57b4d5412b68912097979547d57b5ad8
Nwankwo Henry <nameisifunanya@gmail.com>
2023-07-03 17:10:03 +0100

    Adjusted the structure and created the nav links

--- portfolio-app/app/page.js
+++ portfolio-app/app/page.js
@@ -1,19 +1,26 @@
 import Image from 'next/image';
 import ThemeSwitcher from './themeSwitcher';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center p-24'>
       <header>
-        <nav></nav>
+        <nav>
+          <Link href='#about'>About</Link>
+          <Link href='#portfolio'>Portfolio</Link>
+          <Link href='contact'>Contact</Link>
+        </nav>
         <ThemeSwitcher />
       </header>
-      <section></section>
-      <section></section>
-      <section></section>
-      <section></section>
-      <section></section>
-      <footer></footer>
+      <section id='about'></section>
+      <section id='skills'></section>
+      <section id='portfolio'></section>
+      <section id='experience'></section>
+      <section id='testimonials'></section>
+      <footer>
+        <section id='contact'></section>
+      </footer>
     </main>
   );
 }
