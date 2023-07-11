f8b9b6334f15d9fc71faa228c447096820d0b111
Nwankwo Henry <nameisifunanya@gmail.com>
2023-07-11 09:26:01 +0100

    Adjusted the the dark and light theme looks

--- portfolio-app/app/page.js
+++ portfolio-app/app/page.js
@@ -21,8 +21,8 @@ export default function Home() {
         <div className={source_Serif_4.className}>
           <h1 className='text-white text-3xl'>About</h1>
         </div>
-        <div className='mt-4 flex flex-row w-full outline-1 outline-red-600 outline'>
-          <span className='block border-t-4 border-solid border-orange-400 sm:w-12 w-32 mt-2 mr-2'></span>
+        <div className='mt-4 flex flex-row w-full'>
+          <span className='block border-t-4 border-solid border-green-500 dark:border-orange-400 sm:w-12 w-32 mt-2 mr-2'></span>
           <p className='pa_aboutText '>
             I like to craft solid and scalable frontend products with great user
             experiences. I build accessible, inclusive products and digital
@@ -37,27 +37,27 @@ export default function Home() {
         id='skills'
         className='w-full sm:px-24 px-8 flex flex-wrap gap-6 justify-center items-center'
       >
-        <div className='rounded-md w-44 h-44 bg-gray-800 flex justify-center items-center flex-col hover:cursor-pointer hover:text-black hover:bg-orange-400'>
+        <div className='rounded-md w-44 h-44 shadow-md shadow-gray-200 dark:shadow-none bg-gray-100 dark:bg-gray-800 flex justify-center items-center flex-col hover:cursor-pointer hover:text-black hover:bg-green-200 dark:hover:bg-orange-400'>
           <Image alt='Skill Image'></Image>
           <p className='font-medium'>Skill Name</p>
         </div>
-        <div className='rounded-md w-44 h-44 bg-gray-800 flex justify-center items-center flex-col hover:cursor-pointer hover:text-black hover:bg-orange-400'>
+        <div className='rounded-md w-44 h-44 shadow-md shadow-gray-200 dark:shadow-none bg-gray-100 dark:bg-gray-800 flex justify-center items-center flex-col hover:cursor-pointer hover:text-black hover:bg-green-200  dark:hover:bg-orange-400'>
           <Image alt='Skill Image'></Image>
           <p className='font-medium'>Skill Name</p>
         </div>
-        <div className='rounded-md w-44 h-44 bg-gray-800 flex justify-center items-center flex-col hover:cursor-pointer hover:text-black hover:bg-orange-400'>
+        <div className='rounded-md w-44 h-44 shadow-md shadow-gray-200 dark:shadow-none bg-gray-100 dark:bg-gray-800 flex justify-center items-center flex-col hover:cursor-pointer hover:text-black hover:bg-green-200  dark:hover:bg-orange-400'>
           <Image alt='Skill Image'></Image>
           <p className='font-medium'>Skill Name</p>
         </div>
-        <div className='rounded-md w-44 h-44 bg-gray-800 flex justify-center items-center flex-col hover:cursor-pointer hover:text-black hover:bg-orange-400'>
+        <div className='rounded-md w-44 h-44 shadow-md shadow-gray-200 dark:shadow-none bg-gray-100 dark:bg-gray-800 flex justify-center items-center flex-col hover:cursor-pointer hover:text-black hover:bg-green-200  dark:hover:bg-orange-400'>
           <Image alt='Skill Image'></Image>
           <p className='font-medium'>Skill Name</p>
         </div>
-        <div className='rounded-md w-44 h-44 bg-gray-800 flex justify-center items-center flex-col hover:cursor-pointer hover:text-black hover:bg-orange-400'>
+        <div className='rounded-md w-44 h-44 shadow-md shadow-gray-200 dark:shadow-none bg-gray-100 dark:bg-gray-800 flex justify-center items-center flex-col hover:cursor-pointer hover:text-black hover:bg-green-200  dark:hover:bg-orange-400'>
           <Image alt='Skill Image'></Image>
           <p className='font-medium'>Skill Name</p>
         </div>
-        <div className='rounded-md w-44 h-44 bg-gray-800 flex justify-center items-center flex-col hover:cursor-pointer hover:text-black hover:bg-orange-400'>
+        <div className='rounded-md w-44 h-44 shadow-md shadow-gray-200 dark:shadow-none bg-gray-100 dark:bg-gray-800 flex justify-center items-center flex-col hover:cursor-pointer hover:text-black hover:bg-green-200  dark:hover:bg-orange-400'>
           <Image alt='Skill Image'></Image>
           <p className='font-medium'>Skill Name</p>
         </div>
