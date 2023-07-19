2f2bc7c8aeb991c97b6da75b69b3f475b2d14259
Nwankwo Henry <nameisifunanya@gmail.com>
2023-07-19 12:15:56 +0100

    Finished styling the contact me buttons

--- portfolio-app/app/page.js
+++ portfolio-app/app/page.js
@@ -8,7 +8,13 @@ import {
   HiOutlineExternalLink,
   HiPhone,
 } from 'react-icons/hi';
-import { BsFacebook, BsLinkedin, BsTwitter, BsWhatsapp } from 'react-icons/bs';
+import {
+  BsFacebook,
+  BsGithub,
+  BsLinkedin,
+  BsTwitter,
+  BsWhatsapp,
+} from 'react-icons/bs';
 
 export default function Home() {
   return (
@@ -196,7 +202,7 @@ export default function Home() {
       {/* Contact me section */}
       <section
         id='contact'
-        className='ptf-pri-section flex flex-col items-center relative bg-slate-50 dark:bg-gray-800'
+        className='ptf-pri-section flex flex-col items-center relative bg-slate-50 dark:bg-gray-800 '
       >
         <div className='flex justify-between w-full items-center absolute -top-8 sm:-top-10'>
           <span className='block h-0.5 w-1/5 sm:w-1/3 bg-gradient-to-br from-green-500 to-blue-500 dark:from-red-500 dark:to-orange-400'></span>
@@ -205,21 +211,38 @@ export default function Home() {
           </div>
           <span className='block h-0.5 w-1/5 sm:w-1/3 bg-gradient-to-br from-green-500 to-blue-500 dark:from-red-500 dark:to-orange-400'></span>
         </div>
-        <div>
-          <button>
-            <HiMail /> mymail@gmail.com{' '}
+
+        {/* THe contact me info */}
+        <div className='w-full h-auto sm:w-3/5 sm:h-fit flex flex-col sm:flex-row gap-3 sm:flex-wrap items-center justify-center'>
+          <button className='px-4 py-3 w-full sm:w-fit  bg-gradient-to-br from-green-500 dark:from-slate-900 to-blue-500 dark:to-gray-950 rounded-lg flex gap-2 text-white items-center'>
+            <HiMail className='text-lg dark:text-orange-400' /> mymail@gmail.com{' '}
           </button>
-          <button>
-            <HiPhone />{' '}
+          <button className='px-4 py-3 w-full sm:w-fit  bg-gradient-to-br from-green-500 dark:from-slate-900 to-blue-500 dark:to-gray-950 rounded-lg flex gap-2 text-white items-center'>
+            <HiPhone className='text-lg dark:text-orange-400' />
+            +2347069782561
           </button>
-          <button>
-            <HiLocationMarker />{' '}
+          <button className='px-4 py-3 w-full sm:w-fit  bg-gradient-to-br from-green-500 dark:from-slate-900 to-blue-500 dark:to-gray-950 rounded-lg flex gap-2 text-white items-center'>
+            <HiLocationMarker className='text-lg dark:text-orange-400' />
+            <p>Ilorin, Nigeria</p>
           </button>
         </div>
+
+        {/* The Social Icons */}
         <div className='flex justify-between w-full items-center absolute -bottom-2 sm:-bottom-2'>
           <span className='block h-0.5 w-1/5 sm:w-1/3 bg-gradient-to-br from-green-500 to-blue-500 dark:from-red-500 dark:to-orange-400'></span>
           <div className='flex items-center justify-center gap-3'>
-            <BsFacebook /> <BsTwitter /> <BsLinkedin /> <BsWhatsapp />{' '}
+            <Link href='#' target='_blank'>
+              <BsGithub className='text-lg' />
+            </Link>
+            <Link href='#' target='_blank'>
+              <BsTwitter className='text-lg' />
+            </Link>
+            <Link href='#' target='_blank'>
+              <BsLinkedin className='text-lg' />
+            </Link>
+            <Link href='#' target='_blank'>
+              <BsWhatsapp className='text-lg' />
+            </Link>
           </div>
           <span className='block h-0.5 w-1/5 sm:w-1/3 bg-gradient-to-br from-green-500 to-blue-500 dark:from-red-500 dark:to-orange-400'></span>
         </div>
