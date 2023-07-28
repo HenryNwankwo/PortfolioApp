f46a0861e9a2747c493098e31a653721af5cd0e5
Nwankwo Henry <nameisifunanya@gmail.com>
2023-07-28 12:52:49 +0100

    Moved the testimonial section into components

---
+++ portfolio-app/components/Testimonials.jsx
@@ -0,0 +1,10 @@
+const Testimonials = () => {
+  return (
+    <>
+      {/* Testimonial section */}
+      <section id='testimonials'>This is testimonials</section>
+    </>
+  );
+};
+
+export default Testimonials;
