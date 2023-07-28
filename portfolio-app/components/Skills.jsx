3a823fc302f39856492610ebd4b999bc9d9825aa
Nwankwo Henry <nameisifunanya@gmail.com>
2023-07-28 11:57:22 +0100

    moved the skills section to components

---
+++ portfolio-app/components/Skills.jsx
@@ -0,0 +1,43 @@
+import Image from 'next/image';
+
+const Skills = () => {
+  return (
+    <>
+      {/* The Skills section */}
+
+      <section id='skills' className='py-4'>
+        <div className='text-center py-4 text-2xl sm:text-3xl font-bold'>
+          <h2 className={source_Serif_4.className}>My Skills</h2>
+        </div>
+        <div className='w-full sm:px-24 px-8 flex flex-wrap gap-6 justify-center items-center my-2'>
+          <div className='ptf-skills-box'>
+            <Image alt='Skill Image'></Image>
+            <p className='font-medium'>Skill Name</p>
+          </div>
+          <div className='ptf-skills-box'>
+            <Image alt='Skill Image'></Image>
+            <p className='font-medium'>Skill Name</p>
+          </div>
+          <div className='ptf-skills-box'>
+            <Image alt='Skill Image'></Image>
+            <p className='font-medium'>Skill Name</p>
+          </div>
+          <div className='ptf-skills-box'>
+            <Image alt='Skill Image'></Image>
+            <p className='font-medium'>Skill Name</p>
+          </div>
+          <div className='ptf-skills-box'>
+            <Image alt='Skill Image'></Image>
+            <p className='font-medium'>Skill Name</p>
+          </div>
+          <div className='ptf-skills-box'>
+            <Image alt='Skill Image'></Image>
+            <p className='font-medium'>Skill Name</p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Skills;
