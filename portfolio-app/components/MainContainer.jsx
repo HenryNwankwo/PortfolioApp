e642af9e1e090db95dbae2ff69c02aa5b88e5c8c
Nwankwo Henry <nameisifunanya@gmail.com>
2023-07-28 12:58:00 +0100

    Moved the main container section into components

---
+++ portfolio-app/components/MainContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MainContainer = ({ children }) => {
+  return (
+    <>
+      <main
+        id='main-body'
+        className='flex min-h-screen flex-col items-center w-full h-auto max-w-7xl bg-slate-100 dark:bg-slate-900 dark:text-white transition-all transform-gpu relative mx-auto'
+      >
+        {children}
+      </main>
+    </>
+  );
+};
+
+export default MainContainer;
