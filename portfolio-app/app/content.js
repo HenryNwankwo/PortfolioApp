7ef66c6597f7db4810708d23c1825a0120458ac4
Nwankwo Henry <nameisifunanya@gmail.com>
2023-08-02 10:07:04 +0100

    Created a content file for dynamic contents and added the skills contents

---
+++ portfolio-app/app/content.js
@@ -0,0 +1,97 @@
+export const skills = [
+  {
+    id: 1,
+    name: 'HTML',
+    image: '',
+  },
+  {
+    id: 2,
+    name: 'CSS',
+    image: '',
+  },
+  {
+    id: 3,
+    name: 'Javascript',
+    image: '',
+  },
+  {
+    id: 4,
+    name: 'Bootstrap',
+    image: '',
+  },
+  {
+    id: 5,
+    name: 'Tailwindcss',
+    image: '',
+  },
+  {
+    id: 6,
+    name: 'Reactjs',
+    image: '',
+  },
+  {
+    id: 7,
+    name: 'Nodejs',
+    image: '',
+  },
+  {
+    id: 8,
+    name: 'Excel',
+    image: '',
+  },
+  {
+    id: 9,
+    name: 'Git',
+    image: '',
+  },
+  {
+    id: 10,
+    name: 'Github',
+    image: '',
+  },
+  {
+    id: 11,
+    name: 'Kaggle',
+    image: '',
+  },
+  {
+    id: 12,
+    name: 'Google Sheets',
+    image: '',
+  },
+  {
+    id: 13,
+    name: 'MongoDB',
+    image: '',
+  },
+  {
+    id: 14,
+    name: 'MySQL',
+    image: '',
+  },
+  {
+    id: 15,
+    name: 'Nextjs',
+    image: '',
+  },
+  {
+    id: 16,
+    name: 'Python',
+    image: '',
+  },
+  {
+    id: 17,
+    name: 'R',
+    image: '',
+  },
+  {
+    id: 18,
+    name: 'RStudio',
+    image: '',
+  },
+  {
+    id: 19,
+    name: 'Tableau',
+    image: '',
+  },
+];
