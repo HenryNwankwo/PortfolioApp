90f7b1419d2a3100500e377cec6b21d6076c559c
Nwankwo Henry <nameisifunanya@gmail.com>
2023-08-04 11:12:50 +0100

    Added the sills images into the skill component

--- portfolio-app/components/Skills.jsx
+++ portfolio-app/components/Skills.jsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { skills } from '@/app/content';
 
 const Skills = ({ source_Serif_4 }) => {
   return (
@@ -10,30 +11,17 @@ const Skills = ({ source_Serif_4 }) => {
           <h2 className={source_Serif_4.className}>My Skills</h2>
         </div>
         <div className='w-full sm:px-24 px-8 flex flex-wrap gap-6 justify-center items-center my-2'>
-          <div className='ptf-skills-box'>
-            <Image alt='Skill Image'></Image>
-            <p className='font-medium'>Skill Name</p>
-          </div>
-          <div className='ptf-skills-box'>
-            <Image alt='Skill Image'></Image>
-            <p className='font-medium'>Skill Name</p>
-          </div>
-          <div className='ptf-skills-box'>
-            <Image alt='Skill Image'></Image>
-            <p className='font-medium'>Skill Name</p>
-          </div>
-          <div className='ptf-skills-box'>
-            <Image alt='Skill Image'></Image>
-            <p className='font-medium'>Skill Name</p>
-          </div>
-          <div className='ptf-skills-box'>
-            <Image alt='Skill Image'></Image>
-            <p className='font-medium'>Skill Name</p>
-          </div>
-          <div className='ptf-skills-box'>
-            <Image alt='Skill Image'></Image>
-            <p className='font-medium'>Skill Name</p>
-          </div>
+          {skills.map((skill) => (
+            <div className='ptf-skills-box' key={skill.id}>
+              <Image
+                alt={`${skill.name} logo`}
+                src={skill.image}
+                width={80}
+                height={80}
+              ></Image>
+              <p className='font-medium'>{skill.name}</p>
+            </div>
+          ))}
         </div>
       </section>
     </>
