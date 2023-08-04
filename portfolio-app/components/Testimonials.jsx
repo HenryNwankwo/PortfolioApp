3c7c36d707bbba83e82491b207c799e4a7cf2b7c
Nwankwo Henry <nameisifunanya@gmail.com>
2023-08-04 13:04:14 +0100

    Created a prev and next button for testimonials

--- portfolio-app/components/Testimonials.jsx
+++ portfolio-app/components/Testimonials.jsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { BsCaretLeft, BsCaretRight } from 'react-icons/bs';
 
 const Testimonials = ({ source_Serif_4 }) => {
   return (
@@ -6,8 +7,11 @@ const Testimonials = ({ source_Serif_4 }) => {
       {/* Testimonial section */}
       <section
         id='testimonials'
-        className='ptf-pri-section min-h-90 flex flex-col sm:flex-row'
+        className='ptf-pri-section min-h-90 flex flex-col sm:flex-row relative'
       >
+        <button className='w-12 h-12 rounded-full absolute left-5 sm:left-12 top-1/2 bg-white opacity-40 flex items-center justify-center shadow-2xl shadow-slate-400'>
+          <BsCaretLeft className='text-2xl'></BsCaretLeft>
+        </button>
         <div className='w-full sm:w-2/5 sm:order-2 sm:ml-8'>
           <Image alt='' height={100} width={100} />
         </div>
@@ -38,6 +42,9 @@ const Testimonials = ({ source_Serif_4 }) => {
             </div>
           </div>
         </div>
+        <button className='w-12 h-12 rounded-full absolute right-5 sm:right-12 top-1/2 bg-white opacity-40 flex items-center justify-center shadow-2xl shadow-slate-400'>
+          <BsCaretRight className='text-2xl'></BsCaretRight>
+        </button>
       </section>
     </>
   );
