091f1155efe2605d6284755e7d0a409f036a3d0b
Nwankwo Henry <nameisifunanya@gmail.com>
2023-08-04 09:44:16 +0100

    Changed the favicon to a .ico img and changed the position of the menu burger

--- portfolio-app/components/Header.jsx
+++ portfolio-app/components/Header.jsx
@@ -8,6 +8,12 @@ const Header = () => {
       {/* Header Section */}
 
       <header className='w-full h-20 sm:px-10 px-5 flex items-center justify-between dark:text-slate-400 bg-slate-50 dark:bg-slate-800 sticky top-0 right-0 left-0 z-50 shadow-lg shadow-transparent'>
+        <input
+          type='checkbox'
+          name='menu-burger'
+          id='menu-burger'
+          className='ptf-menu-burger peer sm:hidden checked:mr-5'
+        />
         <div className='flex'>
           <Link href='/' className='flex '>
             <Image alt='Logo'></Image>
@@ -16,12 +22,7 @@ const Header = () => {
             </p>
           </Link>
         </div>
-        <input
-          type='checkbox'
-          name='menu-burger'
-          id='menu-burger'
-          className='ptf-menu-burger peer sm:hidden order-3 checked:mr-5'
-        />
+
         <nav className='absolute hidden w-screen top-20  left-0 shadow-lg shadow-transparent flex-col items-center py-2 bg-slate-50 dark:bg-slate-800 h-auto peer-checked:flex sm:py-0 sm:flex sm:flex-row sm:relative sm:top-0 sm:w-fit '>
           <Link href='#about' className='ptf-nav-links'>
             About
