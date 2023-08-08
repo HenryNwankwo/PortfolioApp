c4a2c6fcaef16f6e305f4da8288ddc8e8bd47cee
Nwankwo Henry <nameisifunanya@gmail.com>
2023-08-08 10:42:28 +0100

    Added a logo to the footer

--- portfolio-app/components/Footer.jsx
+++ portfolio-app/components/Footer.jsx
@@ -1,7 +1,10 @@
+'use client';
 import Link from 'next/link';
 import Image from 'next/image';
+import { usePtfContext } from '@/utils/PtfContext';
 
 const Footer = ({ dancing_script }) => {
+  const { theme } = usePtfContext();
   return (
     <>
       {/* Footer Section */}
@@ -15,7 +18,16 @@ const Footer = ({ dancing_script }) => {
         <div className='w-full flex flex-col sm:flex-row gap-3 items-center justify-center py-4'>
           <div className=''>
             <Link href='/'>
-              <Image alt='Logo'></Image>
+              <Image
+                alt='Logo'
+                width={28}
+                height={28}
+                src={
+                  theme == 'light'
+                    ? '/assets/img/logo-green.png'
+                    : '/assets/img/logo-orange.png'
+                }
+              ></Image>
             </Link>
           </div>
           <p className='text-sm dark:text-slate-400'>
